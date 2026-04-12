a7ebd6adddb6e924a2196864d3f1aa44cb599ec4
Pchelovodova <pchelovodovaes@gmail.com>
2026-04-12 17:28:43 +0200

    Flask. Урок 5. Работа с HTML и подключение шаблонов в Flask

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
-def example():
-    return "For example Hello"
+def home():
+    return render_template("index.html")
 
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
